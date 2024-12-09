8ac4d6de77b0a76e4e8c71e0073db038031483f1
S963-hub <132394552+S963-hub@users.noreply.github.com>
2024-12-09 20:26:53 +0300

    modeller oluşturuldu.

---
+++ GentelmansProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using GentelmansProject.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace GentelmansProject.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Berber> Berbers { get; set; }
+        public DbSet<Kullancilar> Kullancilars { get; set; }
+        public DbSet<Servis> Servises { get; set; }
+        public DbSet<Randevular> Randevulars { get; set; }
+
+    }
+}
